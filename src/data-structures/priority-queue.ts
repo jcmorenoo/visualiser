e9ccc7ac990dd8115e3d2ef90d49a8c3a8741a82
Julian Moreno <jcmorenoo@users.noreply.github.com>
2022-02-25 23:55:58 +1100

    WIP adding some more smarts

--- src/data-structures/priority-queue.ts
+++ src/data-structures/priority-queue.ts
@@ -1,35 +1,57 @@
-interface PriorityQueueItem<T> {
-  priority: number;
-  data: T
+export class PriorityQueueItem<T> {
+  constructor(public priority: number, public data: T) { };
 }
 
-export class PriorityQueue<U, T extends PriorityQueueItem<U>> {
-  private heap: T[];
-  private size: number;
+export class PriorityQueue<T> {
+  private heap: PriorityQueueItem<T>[];
 
-  constructor(){
-    this.size = 0;
+  constructor() {
     this.heap = [];
   }
 
   public peek(): T {
-    if (this.size === 0) {
+    if (this.isEmpty()) {
       throw Error('Queue is empty');
     }
-    return this.heap[0];
+    return this.heap[0].data;
   }
 
-  public push(item: T) {
+  public push(priorityQueueItem: PriorityQueueItem<T>): void;
+  public push(item: T, priority: number): void;
+  public push(item: PriorityQueueItem<T> | T, priority = 0) {
+    let newItem: PriorityQueueItem<T>;
+    if (item instanceof PriorityQueueItem) {
+      newItem = item;
+    }
+    else {
+      newItem = new PriorityQueueItem(priority, item);
+    }
     // insert to the last
+    this.heap.push(newItem);
     // bubble up
-
+    const newItemIndex = this.heap.length - 1;
+    this.bubbleUp(newItemIndex);
   }
 
   public pop(): T {
+    if (this.isEmpty()) {
+      throw Error('Queue is empty');
+    }
     // swap first to last
+    this.swap(0, this.heap.length - 1);
     // remove last
+    const itemToReturn = this.heap.pop();
+    if (!itemToReturn) {
+      throw Error('Invalid state');
+    }
     // bubbledown
+    this.bubbleDown(0);
     // return last;
+    return itemToReturn.data;
+  }
+
+  public isEmpty(): boolean {
+    return this.heap.length === 0;
   }
 
   private bubbleUp(index: number) {
@@ -42,8 +64,14 @@ export class PriorityQueue<U, T extends PriorityQueueItem<U>> {
     // swap with min of left and right
   }
 
+  private swap(firstIndex: number, secondIndex: number) {
+    let temp = this.heap[firstIndex];
+    this.heap[firstIndex] = this.heap[secondIndex];
+    this.heap[secondIndex] = temp;
+  }
+
   private getParentIndex(index: number): number {
-    return Math.floor((index - 1)/2);
+    return Math.floor((index - 1) / 2);
   }
 
   private getLeftIndex(index: number): number {
